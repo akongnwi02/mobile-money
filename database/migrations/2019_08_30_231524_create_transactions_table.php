8726fd2f3516c4fc57af8d4b5cd9e53ac30b7d68
devert <akongnwi02@gmail.com>
2020-05-20 15:37:48 +0100

    Update the transaction table schema

--- database/migrations/2019_08_30_231524_create_transactions_table.php
+++ database/migrations/2019_08_30_231524_create_transactions_table.php
@@ -18,28 +18,33 @@ class CreateTransactionsTable extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->increments('id');
             $table->boolean('is_callback_sent')->default(false);
+            $table->boolean('to_be_verified')->default(false);
             $table->string('callback_url')->default(false);
-            $table->smallInteger('callback_attempts')->nullable();
-            $table->smallInteger('purchase_attempts')->nullable();
+            $table->smallInteger('callback_attempts')->default(0);
+            $table->smallInteger('purchase_attempts')->default(0);
+            $table->smallInteger('verification_attempts')->default(0);
             $table->string('destination');
             $table->float('amount');
             $table->string('service_code');
             $table->string('internal_id');
             $table->string('external_id')->nullable();
-            $table->string('reference')->nullable();
+            $table->string('merchant_id')->nullable();
             $table->string('phone')->nullable();
             $table->string('email')->nullable();
             $table->string('address')->nullable();
             $table->string('name')->nullable();
             $table->string('asset')->nullable();
+            $table->string('error_code')->nullable();
             $table->text('error')->nullable();
+            $table->text('reference')->nullable();
             $table->text('message')->nullable();
             $table->enum('status', [
                 TransactionConstants::CREATED,
                 TransactionConstants::PROCESSING,
                 TransactionConstants::ERRORED,
                 TransactionConstants::SUCCESS,
-                TransactionConstants::FAILED
+                TransactionConstants::FAILED,
+                TransactionConstants::VERIFICATION,
             ]);
             $table->timestamps();
         });
