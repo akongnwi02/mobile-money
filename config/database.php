933e660a39c48fba9ea0fb86b70121184f6679e2
devert <akongnwi02@gmail.com>
2020-05-24 00:40:47 +0100

    Use pgsql database in production

--- config/database.php
+++ config/database.php
@@ -36,10 +36,23 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-    ],
 
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+    ],
+    
      'migrations' => 'migrations',
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
 ];
