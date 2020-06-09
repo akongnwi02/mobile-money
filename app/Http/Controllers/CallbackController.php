751c8dfcb9c2dee512ed6adf95cb304947997f02
devert <akongnwi02@gmail.com>
2020-06-09 19:51:43 +0100

    Updated MTN Callback controller

--- app/Http/Controllers/CallbackController.php
+++ app/Http/Controllers/CallbackController.php
@@ -22,61 +22,6 @@ use Illuminate\Support\Facades\Log;
 
 class CallbackController extends Controller
 {
-    public function mtnCallback(Request $request)
-    {
-        Log::info('New callback request received from MTN service provider', [
-            'ip'      => $request->ip(),
-            'payload' => $request->input()
-        ]);
-        $transaction = Transaction::find();
-        
-        $transactionInLocalDb = [
-            'transaction.status'                => $transaction->status,
-            'transaction.internal_id'           => $transaction->internal_id,
-            'transaction.id'                    => $transaction->id,
-            'transaction.service_code'          => $transaction->service_code,
-            'transaction.created_at'            => $transaction->created_at->toDatetimeString(),
-            'transaction.destination'           => $transaction->destination,
-        ];
-    
-        Log::debug('Transaction exists in local database', $transactionInLocalDb);
-    
-        if (in_array($transaction->status, [
-            TransactionConstants::SUCCESS,
-            TransactionConstants::FAILED,
-        ])) {
-            Log::emergency('Transaction in final status received a status update',[
-                'status received'                   => $request->input('status'),
-                'transaction.status'                => $transaction->status,
-                'transaction.uuid'                  => $transaction->uuid,
-                'transaction.code'                  => $transaction->code,
-                'transaction.service_code'          => $transaction->service_code,
-                'transaction.movement_code'         => $transaction->movement_code,
-                'transaction.paymentaccount'        => $transaction->paymentaccount,
-                'transaction.created_at'            => $transaction->created_at->toDatetimeString(),
-                'transaction.destination'           => $transaction->destination,
-                'transaction.total_customer_amount' => $transaction->total_customer_amount,
-            ]);
-            throw new ServerErrorException(BusinessErrorCodes::TRANSACTION_IN_FINAL_STATUS, "Transaction $transaction->code in final state received a status update");
-        };
-    
-        $transaction->status         = $request->input('status');
-        $transaction->asset          = $request->input('asset');
-        $transaction->message        = $request->input('message');
-        $transaction->error_code     = $request->input('error_code');
-        $transaction->to_be_verified = $request->input('to_be_verified');
-    
-        $transaction->save();
-    
-        Log::info('Inserting transaction to COMPLETE queue');
-    
-        dispatch(new CallbackJob($transaction))->onQueue(QueueConstants::CALLBACK_QUEUE);
-    
-        return response()->json([
-            'status' => 'OK'
-        ], 200);
-    }
-    
     /**
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
@@ -84,21 +29,21 @@ class CallbackController extends Controller
      * @throws GeneralException
      * @throws NotFoundException
      */
-    public function orangeWpCallback(Request $request)
+    public function mtnCallback(Request $request)
     {
-        Log::info("{$this->getClassName()}: New callback request received from Orange web payment service provider", [
+        Log::info("{$this->getClassName()}: New callback request received from MTN MOMO service provider", [
             'ip'      => $request->ip(),
             'payload' => $request->input()
         ]);
     
-        $transaction = Transaction::where('merchant_id', $request->input('payToken'))->first();
-        
+        $transaction = Transaction::where('internal_id', $request->input('externalId'))->first();
+    
         if (! $transaction) {
-            Log::emergency("{$this->getClassName()}: Received status update from Orange web payment for a transaction which does not exist in the local system", [
+            Log::emergency("{$this->getClassName()}: Received status update from MTN MOMO for a transaction which does not exist in the local system", [
                 'payload' => $request->input()
             ]);
-    
-            throw new NotFoundException(ErrorCodesConstants::TRANSACTION_NOT_FOUND, 'Sending status update for a transaction not existing in local system');
+        
+            throw new NotFoundException(ErrorCodesConstants::TRANSACTION_NOT_FOUND, "The transaction {$request->input('externalId')} does not exist in our system");
         }
     
         $transactionInLocalDb = [
@@ -124,7 +69,90 @@ class CallbackController extends Controller
                 'transaction.created_at'            => $transaction->created_at->toDatetimeString(),
                 'transaction.destination'           => $transaction->destination,
             ]);
-            throw new GeneralException(ErrorCodesConstants::TRANSACTION_IN_FINAL_STATUS, "Transaction $transaction->id in final state received a status update");
+            throw new GeneralException(ErrorCodesConstants::TRANSACTION_IN_FINAL_STATUS, "Transaction $transaction->internal_id is already in final status $transaction->status");
+        };
+    
+        $transaction->message = 'Transaction status updated via callback';
+        $transaction->merchant_id = $request->input('financialTransactionId');
+    
+        if ($request->input('status') == 'SUCCESSFUL') {
+            $transaction->status = TransactionConstants::SUCCESS;
+        }
+    
+        /**
+         * Make sure the status is not a premature status before failing the transaction if status is not successful
+         */
+        else if (in_array($request->input('status'), ['PENDING'])) {
+            Log::warning("{$this->getClassName()}: Premature status received from MTN MOMO provider", [
+                'status received' => $request->input('status'),
+                'transaction in local system' => $transactionInLocalDb
+            ]);
+        
+            throw new BadRequestException(ErrorCodesConstants::GENERAL_CODE, "We cannot update the transaction {$request->input('externalId')} with status {$request->input('status')}");
+        }
+        else {
+            $transaction->status = TransactionConstants::FAILED;
+            $transaction->error_code = ErrorCodesConstants::GENERAL_CODE;
+            $transaction->error = $request->input('reason');
+        }
+    
+        $transaction->save();
+    
+        Log::info("{$this->getClassName()}: Inserting transaction to CALLBACK queue");
+    
+        dispatch(new CallbackJob($transaction))->onQueue(QueueConstants::CALLBACK_QUEUE);
+    
+        return $this->successResponse();
+    }
+    
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws BadRequestException
+     * @throws GeneralException
+     * @throws NotFoundException
+     */
+    public function orangeWpCallback(Request $request)
+    {
+        Log::info("{$this->getClassName()}: New callback request received from Orange web payment service provider", [
+            'ip'      => $request->ip(),
+            'payload' => $request->input()
+        ]);
+    
+        $transaction = Transaction::where('merchant_id', $request->input('payToken'))->first();
+        
+        if (! $transaction) {
+            Log::emergency("{$this->getClassName()}: Received status update from Orange web payment for a transaction which does not exist in the local system", [
+                'payload' => $request->input()
+            ]);
+    
+            throw new NotFoundException(ErrorCodesConstants::TRANSACTION_NOT_FOUND, "The transaction {$request->input('status')} does not exist in our system");
+        }
+    
+        $transactionInLocalDb = [
+            'transaction.status'                => $transaction->status,
+            'transaction.internal_id'           => $transaction->internal_id,
+            'transaction.id'                    => $transaction->id,
+            'transaction.service_code'          => $transaction->service_code,
+            'transaction.created_at'            => $transaction->created_at->toDatetimeString(),
+            'transaction.destination'           => $transaction->destination,
+        ];
+        Log::info("{$this->getClassName()}: Transaction exists in the local system with the following value", $transactionInLocalDb);
+    
+    
+        if (in_array($transaction->status, [
+            TransactionConstants::SUCCESS,
+            TransactionConstants::FAILED,
+        ])) {
+            Log::emergency("{$this->getClassName()}: Transaction in final status received a status update",[
+                'transaction.status'                => $transaction->status,
+                'transaction.internal_id'           => $transaction->internal_id,
+                'transaction.id'                    => $transaction->id,
+                'transaction.service_code'          => $transaction->service_code,
+                'transaction.created_at'            => $transaction->created_at->toDatetimeString(),
+                'transaction.destination'           => $transaction->destination,
+            ]);
+            throw new GeneralException(ErrorCodesConstants::TRANSACTION_IN_FINAL_STATUS, "Transaction $transaction->merchant_id is already in final status $transaction->status");
         };
     
         $transaction->message = $request->input('message');
@@ -156,13 +184,18 @@ class CallbackController extends Controller
     
         dispatch(new CallbackJob($transaction))->onQueue(QueueConstants::CALLBACK_QUEUE);
     
-        return response()->json([
-            'status' => 'OK'
-        ], 200);
+        return $this->successResponse();
     }
     
     public function getClassName()
     {
         return class_basename($this);
+    }
+    
+    public function successResponse()
+    {
+        return response()->json([
+            'status' => 'OK'
+        ], 200);
     }
 }
