66f3448acb9bcffdcd019a131a6609a2bd57d2fa
devert <akongnwi02@gmail.com>
2020-10-15 00:51:35 +0100

    Add the migration script

---
+++ database/migrations/2020_10_13_155211_create_authentication_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthenticationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authentications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('access_token');
+            $table->string('refresh_token')->nullable();
+            $table->bigInteger('expires_in')->nullable();
+            $table->string('service_code');
+            $table->string('type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authentication');
+    }
+}
